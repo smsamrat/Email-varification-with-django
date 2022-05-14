5dcda16b2b4c479d75ae6c843bda9254ea54057e
you <97931972+smsamrat@users.noreply.github.com>
2022-05-14 13:23:08 +0600

    password reset
    create 4 html file and 4 urls with import password reset library

--- session/urls.py
+++ session/urls.py
@@ -1,10 +1,18 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from . import views
+import session
 urlpatterns = [
    path('',views.home, name='home'),
    path('signup/',views.signup, name='signup'),
    path('login/',views.login_user, name='login'),
    path('logout/',views.logout_user, name='logout'),
+
+   #password reset url
+   path('reset/password/',PasswordResetView.as_view(template_name = 'session/reset_pass.html'), name='password_reset'),
+   path('reset/password/done/',PasswordResetDoneView.as_view(template_name = 'session/reset_pass_done.html'), name='password_reset_done'),
+   path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name = 'session/password_reset_confirm.html'), name='password_reset_confirm'),
+   path('reset/done/',PasswordResetCompleteView.as_view(template_name = 'session/password_reset_complete.html'), name='password_reset_complete'),
 ]
